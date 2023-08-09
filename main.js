6e99aa78a3ec260647e0f5f881107857d741c770
ManC <chhetrimandip7@gmail.com>
2023-08-09 12:41:07 -0500

    add time, date, and css transitions

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+//all variables 
 const lightElement = document.getElementById('lightBox');
 const darkElement = document.getElementById('darkBox');
 const bodyElement = document.querySelector('body');
+const containerElement = document.getElementsByClassName("container");
+const clock = document.getElementById('time');
+const day = document.getElementById('date');
 //will create black box when hovering over
+/*function sizeChange(){
+    containerElement.style.height = '40vh';  
+}*/
 function lightHover(){
     lightElement.style.backgroundColor = 'black';
     lightElement.style.padding= '20%';
@@ -31,3 +38,11 @@ darkElement.addEventListener('mouseenter', darkHover);
 darkElement.addEventListener('mouseleave',darkUnhover);
 lightElement.addEventListener('click',normalColor);
 darkElement.addEventListener('click',backColorChange);
+//containerElement.addEventListener('mouseenter',sizeChange);
+//adding time functionality to page
+setInterval(() => {
+    const currentTime = new Date();
+    clock.innerHTML = currentTime.toLocaleTimeString();
+})
+
+day.innerHTML = new Date().toDateString();
